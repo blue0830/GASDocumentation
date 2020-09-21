c0da53d0a868c8d1e27de7041025444db1cd6ada
blue0830 <blue0830@vip.qq.com>
2020-09-22 00:01:50 +0800

    fix bugs

--- Source/GASDocumentation/Private/Characters/Pickupable.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright 2019 Dan Kestranek.
-
-
-#include "Pickupable.h"
-#include "GDHeroCharacter.h"
-
-// Add default functionality here for any IPickupable functions that are not pure virtual.
--- Source/GASDocumentation/Private/Custom/Abilities/Bomb.cpp
+++ Source/GASDocumentation/Private/Custom/Abilities/Bomb.cpp
@@ -12,7 +12,7 @@ ABomb::ABomb()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
+	SetReplicates(true);
 	SphereCollision = CreateDefaultSubobject<USphereComponent>(FName("SphereCollision"));
 	SphereCollision->SetupAttachment(RootComponent);
 	SphereCollision->SetSphereRadius(51);
